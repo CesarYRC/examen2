e4d1aae559f94892b593acccd1414a0041406663
CesarYRC <creyes17@ucol.mx>
2024-04-17 13:22:35 -0600

    first commit

---
+++ ExamenSegundaParcial/ExamenSegundaParcial/Data/Producto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Producto
+{
+    [Required(ErrorMessage = "El número de inventario no debe estar vació")]
+    [Range(1, int.MaxValue, ErrorMessage = "Debe ser entero positivo mayor que 0.")]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "El nombre no debe estar vació.")]
+    public string? Nombre { get; set; }
+    [Required(ErrorMessage = "Debe seleccionar departamento.")]
+    public string? Departamento { get; set; }
+}
